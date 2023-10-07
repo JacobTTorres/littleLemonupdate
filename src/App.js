7abb1b8a8e4aeaa699495104c8e0ec5c7415cfbe
JacobTTorres <jacobttorres@hotmail.com>
2023-10-06 22:19:45 -0500

    fixed menu and ui issues

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 
 export default function App() {
-  return (
-    <>
-      <div className="outterApp">
-        <div className="headerBackdrop"></div>
-        <div className="appContainer">
-          <Nav />
-          <Header />
-          <Main />
-          <Footer />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="outterApp"></div>
+			<div className="headerBackdrop"></div>
+			<div className="appContainer">
+				<Nav />
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+		</>
+	);
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,33 +5,33 @@ import DropDownMenu from './DropDownMenu';
 import './nav.css';
 
 export default function Nav() {
-  const options = ['Option 1'];
-  const [selectedOption, setSelectedOption] = useState(null);
+	const options = ['Option 1'];
+	const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleSelect = (option) => {
-    setSelectedOption(option);
-  };
+	const handleSelect = (option) => {
+		setSelectedOption(option);
+	};
 
-  return (
-    <nav className="topNavbar">
-      <div className="navContainer">
-        <img
-          src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/logo.5b4e652aff88b09ef8f1d465f3db445b.svg"
-          alt="Little Lemon img"
-          className="logo"
-        />
-        <ul className="navLinks">
-          <li className="links">Home</li>
-          <li className="links">About</li>
-          <li className="links">Menu</li>
-          <li className="links">Reservations</li>
-          <li className="links">Order Online</li>
-          <li className="links">Login</li>
-        </ul>
-        <button className="navBtn">
-          <DropDownMenu options={options} onSelect={handleSelect} />
-        </button>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="topNavbar">
+			<div className="navContainer">
+				<img
+					src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/logo.5b4e652aff88b09ef8f1d465f3db445b.svg"
+					alt="Little Lemon img"
+					className="logo"
+				/>
+				<ul className="navLinks">
+					<li className="links">Home</li>
+					<li className="links">About</li>
+					<li className="links">Menu</li>
+					<li className="links">Reservations</li>
+					<li className="links">Order Online</li>
+					<li className="links">Login</li>
+				</ul>
+				<button className="navBtn">
+					<DropDownMenu options={options} onSelect={handleSelect} />
+				</button>
+			</div>
+		</nav>
+	);
 }
